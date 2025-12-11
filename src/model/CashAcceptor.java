58345d3fd42ee76f851bdad6414bbb0065e72112
AdiletG <adiletg844@gmail.com>
2025-12-11 21:53:17 +0600

    Начал делать ДЗ по добавлению оплаты купюрой

---
+++ src/model/CashAcceptor.java
@@ -0,0 +1,17 @@
+package model;
+
+public class CashAcceptor {
+    private int cash;
+
+    public CashAcceptor(int cash) {
+        this.cash = cash;
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    public void setCash(int amount) {
+        this.cash = cash;
+    }
+}
